d1e95b3f39066395a522d6577137c9f47e7df469
your Name <your@email>
2023-11-27 14:17:13 +0700

    tan

---
+++ ExportWordFileFromTemplate/ExportWordFileFromTemplate/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ExportWordFileFromTemplate
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnXuatFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtSdt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(57, 12);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(345, 20);
+            this.txtHoTen.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Họ tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Đia chỉ";
+            // 
+            // btnXuatFile
+            // 
+            this.btnXuatFile.BackColor = System.Drawing.Color.Green;
+            this.btnXuatFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnXuatFile.ForeColor = System.Drawing.Color.White;
+            this.btnXuatFile.Location = new System.Drawing.Point(16, 109);
+            this.btnXuatFile.Name = "btnXuatFile";
+            this.btnXuatFile.Size = new System.Drawing.Size(386, 37);
+            this.btnXuatFile.TabIndex = 3;
+            this.btnXuatFile.Text = "Xuất file";
+            this.btnXuatFile.UseVisualStyleBackColor = false;
+            this.btnXuatFile.Click += new System.EventHandler(this.btnXuatFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SĐT";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(57, 42);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(345, 20);
+            this.txtDiaChi.TabIndex = 0;
+            // 
+            // txtSdt
+            // 
+            this.txtSdt.Location = new System.Drawing.Point(57, 71);
+            this.txtSdt.Name = "txtSdt";
+            this.txtSdt.Size = new System.Drawing.Size(345, 20);
+            this.txtSdt.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 165);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnXuatFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSdt);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtHoTen);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Kết xuất file word từ file mẫu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnXuatFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtSdt;
+    }
+}
+
---
+++ ExportWordFileFromTemplate/ExportWordFileFromTemplate/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Novacode;
+
+namespace ExportWordFileFromTemplate
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnXuatFile_Click(object sender, EventArgs e)
+        {
+            DocX gDoc;
+
+            try
+            {
+                if (File.Exists(@"Test.docx"))
+                {
+                    gDoc = CreateInvoiceFromTemplate(DocX.Load(@"Test.docx"));
+                    gDoc.SaveAs(@"newFile.docx");
+                }
+                else
+                {
+                    MessageBox.Show("Không có file Test.docx");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private DocX CreateInvoiceFromTemplate(DocX template)
+        {
+            template.AddCustomProperty(new CustomProperty("HoTen", txtHoTen.Text));
+            template.AddCustomProperty(new CustomProperty("DiaChi", txtDiaChi.Text));
+            template.AddCustomProperty(new CustomProperty("Sdt", txtSdt.Text));
+
+            //table
+            var t = template.Tables[0];
+            CreateAndInsertInvoiceTableAfter(t, ref template);
+            t.Remove();
+            return template;
+        }
+
+        private Table CreateAndInsertInvoiceTableAfter(Table t, ref DocX document)
+        {
+            var data = GetDataFromDatabase();
+
+            var invoiceTable = t.InsertTableAfterSelf(data.Rows.Count + 1, data.Columns.Count);
+            invoiceTable.Design = TableDesign.DarkListAccent1;
+
+            var tableTitle = new Formatting();
+            tableTitle.Bold = true;
+
+            invoiceTable.Rows[0].Cells[0].InsertParagraph("Họ tên", false, tableTitle);
+            invoiceTable.Rows[0].Cells[1].InsertParagraph("Địa chỉ", false, tableTitle);
+            invoiceTable.Rows[0].Cells[2].InsertParagraph("SĐT", false, tableTitle);
+            for (var row = 1; row < invoiceTable.RowCount; row++)
+            {
+                for (var cell = 0; cell < invoiceTable.ColumnCount; cell++)
+                {
+                    invoiceTable.Rows[row].Cells[cell].InsertParagraph(data.Rows[row - 1].ItemArray[cell].ToString(), false);
+                }
+            }
+            return invoiceTable;
+        }
+
+        private DataTable GetDataFromDatabase()
+        {
+            var table = new DataTable();
+            table.Columns.AddRange(new DataColumn[]
+            {
+                new DataColumn("HoTen"),
+                new DataColumn("DiaChi"),
+                new DataColumn("SĐT") 
+            });
+
+            table.Rows.Add("Nguyễn văn Tùng", "hà nội", "0986110192");
+            table.Rows.Add("Nguyễn thị tuyết Ngân", "hà nội", "0986110192");
+
+            return table;
+        }
+    }
+}
